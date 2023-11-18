cd898c240233333fd667830239931d68efb3b9e9
jaxg93 <112910984+jaxg93@users.noreply.github.com>
2023-11-18 14:01:46 -0700

    Update news-api.js

--- IAintReadingAllThat/includes/news-api.js
+++ IAintReadingAllThat/includes/news-api.js
@@ -1,6 +1,10 @@
 //Get top three headlines
 //Specify country, topic, keywords
 
+
+ApiKey = "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"
+
+
 function MyConstructor() {
     // Property
     this.propertyName = "propertyValue";
