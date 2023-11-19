5d6c207206d14ed16781fe5e3b8074a0c9c777ce
Reonel Duque <rduque89@yahoo.com>
2023-11-18 19:24:54 -0700

    created a navbar and a separate testing

---
+++ IAintReadingAllThat/news-pages/categoryList.js
@@ -0,0 +1,23 @@
+const CATEGORY_LIST = [
+    {
+        category: "business"
+    },
+    {
+        category: "entertainment"
+    },
+    {
+        category: "general"
+    },
+    {
+        category: "health"
+    },
+    {
+        category: "science"
+    },
+    {
+        category: "sports"
+    },
+    {
+        category: "technology"
+    }
+]
