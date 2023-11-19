677703da07a6ef1b041661f731dbbb6b771b80c3
Reonel Duque <rduque89@yahoo.com>
2023-11-18 20:56:27 -0700

    Update NavBar.js

--- IAintReadingAllThat/news-pages/components/NavBar.js
+++ IAintReadingAllThat/news-pages/components/NavBar.js
@@ -6,10 +6,10 @@ import Typography from '@mui/material/Typography'
 
 
 export default function NavBar(props) {
-  return <AppBar position="static">
+  return <AppBar position="static" sx={{backgroundColor: "#313e5a"}} >
     <Toolbar>
       <Link href="/openAiTest/openAiTest">
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        <Typography variant="h6" component="div" >
           Testing AI
       </Typography></Link>    
     </Toolbar>
