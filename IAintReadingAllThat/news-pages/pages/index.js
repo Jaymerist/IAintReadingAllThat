cdb4438da435157ea707b8bf3bc4e7e4269ba750
Reonel Duque <rduque89@yahoo.com>
2023-11-18 20:41:24 -0700

    Update index.js

--- IAintReadingAllThat/news-pages/pages/index.js
+++ IAintReadingAllThat/news-pages/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
   return <div>
     <Head>
-      <title>'I Ain't Reading All that'</title>
+      <title>"I Aint Reading All that"</title>
       <meta name="description" content="I Ain't reading all that" />
       <link rel="icon" href="/favicon.ico" />
     </Head>
